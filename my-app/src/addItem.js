e42ac1c5e3126a54b9d5f25877e964fbf6e0e0d4
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-24 16:16:56 +0200

    basic file structure

---
+++ my-app/src/addItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AddItem(){
+
+
+
+    return (
+        <div>
+        <ul></ul>
+        <button>done</button>
+        <button>edit</button>
+        <button>edit</button>
+        </div>
+    )
+}
